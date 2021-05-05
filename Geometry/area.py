92e3a13985ad08121b9319307690c21e9b3b7c56
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-05-05 12:37:55 +0530

    Package at other loaction

--- Geometry/area.py
+++
@@ -1,8 +0,0 @@
-def rectangle(a,b):
-    return a*b
-
-def triangle(a,b):
-    return (a*b)/2
-
-def print1():
-    print("smnd kjn")
--- Geometry/shapes.py
+++
@@ -1,5 +0,0 @@
-import Geometry.area as dp
-
-
-print(dp.rectangle(10,9))
-print(dp.triangle(10,9))
